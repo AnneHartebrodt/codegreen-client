5f8f7516c84a15cdb099161137826a612567dd21
Anne Hartebrodt <anne.hartebrodt@fau.de>
2023-07-19 15:42:15 +0200

    modify toml

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@ import sys
 sys.path.insert(0, os.path.abspath('../..'))
 # -- Project information -----------------------------------------------------
 
-project = 'code-green-client'
+project = 'codegreen-client'
 copyright = '2023, Anne Hartebrodt'
 author = 'Anne Hartebrodt'
 
@@ -30,8 +30,8 @@ release = '0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['myst_parser',  
-              'sphinx.ext.autodoc',
-            'sphinx.ext.autosummary']
+                'sphinx.ext.autodoc',
+                'sphinx.ext.autosummary']
 
 autosummary_generate = True  # Turn on sphinx.ext.autosummary
 # Add any paths that contain templates here, relative to this directory.
